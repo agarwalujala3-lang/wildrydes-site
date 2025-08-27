ffc747376efd49e300b22b616289492ca49a04d2
agarwalujala3-lang <agarwalujala3@gmail.com>
2025-08-27 15:58:40 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-north-1_rqQXCJ639', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3vc84drokesecoi2irss2f5icr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '6sljr0a0rjj8p0s4l8n81nv9ik', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
